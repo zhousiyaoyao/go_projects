04de44ff699bf96ebc842145f96f33b0f4ff15b1
zhousiyao <siyao.zhou@shopee.com>
2023-10-23 18:45:55 +0800

    chore: example 1

--- gee-web/main.go
+++ gee-web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -9,6 +10,16 @@ func indexHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
 }
 
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	for k, v := range req.Header {
+		fmt.Fprintf(w, "Header [%q] = %q\n", k, v)
+	}
+}
+
+
+
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/hello", helloHandler)
+	log.Fatal(http.ListenAndServe(":9999", nil))
 }
