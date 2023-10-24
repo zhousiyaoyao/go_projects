3e8b882bb98ee22f40914392127621596cd602ac
zhousiyao <siyao.zhou@shopee.com>
2023-10-24 17:28:32 +0800

    feat: day two context

--- gee-web/base3/main.go
+++ gee-web/base3/main.go
@@ -1,21 +1,27 @@
 package main
 
 import (
-	"fmt"
-	"gee"
 	"net/http"
+
+	"gee"
 )
 
 func main() {
 	r := gee.New()
-	r.GET("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "URL.path = %q\n", req.URL.Path)
+
+	r.GET("/", func(c *gee.Context) {
+		c.HTML(http.StatusOK, "<h1>hello</h1>")
 	}) 
 
-	r.GET("/hello", func(w http.ResponseWriter, req *http.Request) {
-		for k, v := range req.Header {
-			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
-		}
+	r.GET("/hello", func(c *gee.Context) {
+		c.String(http.StatusOK, "hello %s, you are at %s\n", c.Query("name"), c.Path)
+	})
+
+	r.POST("/login", func(c *gee.Context) {
+		c.JSON(http.StatusOK, gee.H{
+			"username": c.PostForm("username"),
+			"password": c.PostForm("password"),
+		})
 	})
 
 	r.Run(":9999")
