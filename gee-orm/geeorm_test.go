cf282c9452e61d2db751eff73495c65e55e7235b
zhousiyaoyao <1094290934@qq.com>
2023-10-26 11:48:23 +0800

    feat: orm day one

---
+++ gee-orm/geeorm_test.go
@@ -0,0 +1,21 @@
+package geeorm
+
+import (
+	"testing"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func OpenDB(t *testing.T) *Engine {
+	t.Helper()
+	engine, err := NewEngine("sqlite3", "gee.db")
+	if err != nil {
+		t.Fatal("failed to connect", err)
+	}
+	return engine
+}
+
+func TestNewEngine(t *testing.T) {
+	engine := OpenDB(t)
+	defer engine.Close()
+}
---
+++ gee-orm/go.mod
@@ -0,0 +1,5 @@
+module geeorm
+
+go 1.21.1
+
+require github.com/mattn/go-sqlite3 v2.0.3+incompatible
